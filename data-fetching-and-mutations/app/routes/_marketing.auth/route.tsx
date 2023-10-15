971004672a68c6d4116cba4f08ddb8e87af9053f
Gaurav Gupta <gouravgupta840@gmail.com>
2023-10-15 13:22:07 +0530

    Extracting search parameters

--- data-fetching-and-mutations/app/routes/_marketing.auth/route.tsx
+++ data-fetching-and-mutations/app/routes/_marketing.auth/route.tsx
@@ -1,10 +1,25 @@
 import authStyles from "~/styles/auth.css";
 import AuthForm from '~/components/auth/AuthForm';
+import { ActionFunction } from "@remix-run/node";
 
 export default function AuthPage() {
   return (
     <AuthForm />
   );
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get('mode') || 'login';
+
+  const formData = await request.formData()
+  const credential = Object.fromEntries(formData);
+  
+  if (authMode === 'login') {
+    // login logic
+  } else {
+    // signup logic (create user)
+  }
 }
 
 export function links() {
