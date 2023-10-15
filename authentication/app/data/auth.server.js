510789ba723ed5961b7629b21bd644f20053a38a
Gaurav Gupta <gouravgupta840@gmail.com>
2023-10-15 20:53:37 +0530

    wrote login login

--- authentication/app/data/auth.server.js
+++ authentication/app/data/auth.server.js
@@ -1,14 +1,14 @@
-import pkg from 'bcryptjs';
-const { hash } = pkg;
+import pkg from "bcryptjs";
+const { hash, compare } = pkg;
 
-import { prisma } from './database.server';
+import { prisma } from "./database.server";
 
 export async function signup({ email, password }) {
   const existingUser = await prisma.user.findFirst({ where: { email } });
 
   if (existingUser) {
     const error = new Error(
-      'A user with the provided email address exists already.'
+      "A user with the provided email address exists already."
     );
     error.status = 422;
     throw error;
@@ -17,4 +17,25 @@ export async function signup({ email, password }) {
   const passwordHash = await hash(password, 12);
 
   await prisma.user.create({ data: { email: email, password: passwordHash } });
+}
+
+export async function login({ email, password }) {
+  const existingUser = await prisma.user.findFirst({ where: { email } });
+
+  if (!existingUser) {
+    const error = new Error(
+      "Cloud not log you in, please check the provided credentials."
+    );
+    error.status = 401;
+    throw error;
+  }
+
+  const passwordCorrect = await compare(password, existingUser.password);
+  if (!passwordCorrect) {
+    const error = new Error(
+      "Cloud not log you in, please check the provided credentials."
+    );
+    error.status = 401;
+    throw error;
+  }
 }
