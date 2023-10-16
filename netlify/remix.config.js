c6070eebd2770f6227ce1611c98bc40334669ff0
Gaurav Gupta <gouravgupta840@gmail.com>
2023-10-16 13:31:43 +0530

    configure the app

--- netlify/remix.config.js
+++ netlify/remix.config.js
@@ -14,7 +14,12 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // See https://remix.run/docs/en/main/file-conventions/route-files-v2
   future: {
+    v2_normalizeFormMethod: true,
     v2_routeConvention: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_dev: true
   }
   // add your own custom config here if you want to.
   //
